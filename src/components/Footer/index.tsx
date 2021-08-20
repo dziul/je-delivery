e25b1a7d6aed6db65fc10781ff461ed2f18503e9
luiz carlos wagner dos santos <luizcarloswagner@gmail.com>
2021-08-20 15:34:07 -0300

    feat: :sparkles: add component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container } from '~/components/UI'
+
+import './styles.scss'
+
+const Footer: React.FC = () => {
+  return (
+    <div className='c-footer'>
+      <Container>footer</Container>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './styles.scss'
+
+const Loading: React.FC = () => {
+  return <div className='c-loading'>carregando...</div>
+}
+
+export default Loading
